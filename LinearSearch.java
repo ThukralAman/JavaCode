a4abcff8876762753d1ec130334735c03002dd2c
Aman <amanthukral1991@gmail.com>
2016-10-28 03:06:11 +0530

    LinearSearch in java program added

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,16 +1,25 @@
+import java.util.Scanner;
 
 public class LinearSearch {
-	
-	public static void main(String args[]){
-		int a[] = {20, 32, 90, 1, 13, 7, 88, 60};
-		
-		float rate = 5.8f;
-		System.out.println(rate);
-		System.out.println('a' + 3);
-		
-		for (String k:args){
-			System.out.println(k);
+
+	public static void main(String args[]) {
+		int arr[] = { 20, 32, 90, 1, 13, 7, 88, 60 };
+		boolean flag = false;
+		Scanner scan = new Scanner(System.in);
+		int num_to_find = scan.nextInt();
+
+		for (int num : arr) {
+			if (num == num_to_find) {
+				flag = true;
+				break;
+			}
 		}
-		
+
+		if (flag) {
+			System.out.println("Number: " + num_to_find + "is present in List!");
+		} else {
+			System.out.println("Number: " + num_to_find + "is NOT present in List :(");
+		}
+		scan.close();
 	}
 }
