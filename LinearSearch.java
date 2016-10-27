e8722038b8f8ae83733ce591ce5ab771657627c4
Aman <amanthukral1991@gmail.com>
2016-10-28 02:09:54 +0530

    Test commit to personal repo

---
+++ LinearSearch.java
@@ -0,0 +1,16 @@
+
+public class LinearSearch {
+	
+	public static void main(String args[]){
+		int a[] = {20, 32, 90, 1, 13, 7, 88, 60};
+		
+		float rate = 5.8f;
+		System.out.println(rate);
+		System.out.println('a' + 3);
+		
+		for (String k:args){
+			System.out.println(k);
+		}
+		
+	}
+}
