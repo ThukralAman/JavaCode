c302d2bb4b583a689da94b42f7f69ab77e6ba147
Aman <amanthukral1991@gmail.com>
2017-03-07 23:30:46 +0530

    heap creation code
    heap creation code

---
+++ Heap.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+/* 
+ * 
+ * Property(Max Heap): Parent is always greater than its child + tree is almost complete binary tree with leaves being added from left most node
+ *  - Can be represented in array with:
+ *	- parent i=0, child1 = (2*i+1) child2 =(2*i+2)
+ * */
+
+
+public class Heap {
+	public static void main(String args[]) {
+		Heap h = new Heap();
+		int num[] = {33, 2, 198, 89, 1004, 65, 56789, 7, 43, 765};
+		int n = num.length;
+		
+		
+		
+		for(int i=0; i<num.length; i++) {
+			System.out.println(num[i] + ',');
+		}
+		
+		for(int i=((n/2)-1); i>=0; i--) {
+			h.heapify(num, i, n);
+		}
+		
+		System.out.println("After heapifying:");
+		
+		for(int i=0; i<num.length; i++) {
+			System.out.println(num[i] + ',');
+		}
+	}
+	
+	
+	public void heapify(int num[], int i, int n) {
+		Utils utils = new Utils();
+		int largest = i,
+				l = 2*i+1,
+				r = 2*i+2;
+		 
+		if( l<n && num[largest] < num[l]){
+			largest = l;
+		}
+		
+		if(r<n  && num[largest] < num[r]) {
+			largest = r;
+		}
+		
+		if(largest!=i) {
+			utils.swap(num, i, largest);
+			heapify(num, largest, n);
+		}
+	}
+}
