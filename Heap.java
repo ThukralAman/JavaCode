8cfa74cf28e8355861372b148af369e80e449200
Aman <amanthukral1991@gmail.com>
2017-03-08 20:21:09 +0530

    Removed data creation in main method

--- Heap.java
+++ Heap.java
@@ -9,29 +9,41 @@ import java.util.Scanner;
 
 
 public class Heap {
+	int num[] = {33, 2, 198, 89, 1004, 65, 56789, 7, 43, 765};
+	
 	public static void main(String args[]) {
 		Heap h = new Heap();
-		int num[] = {33, 2, 198, 89, 1004, 65, 56789, 7, 43, 765};
-		int n = num.length;
+		/*int num[] = {33, 2, 198, 89, 1004, 65, 56789, 7, 43, 765};
+		int n = num.length;*/
 		
+		h.printHeapArray();
+		h.createHeap();
 		
-		
-		for(int i=0; i<num.length; i++) {
+		/*for(int i=0; i<num.length; i++) {
 			System.out.println(num[i] + ',');
-		}
+		}*/
 		
-		for(int i=((n/2)-1); i>=0; i--) {
+		/*for(int i=((n/2)-1); i>=0; i--) {
 			h.heapify(num, i, n);
-		}
+		}*/
 		
 		System.out.println("After heapifying:");
-		
+		System.out.println();
+		h.printHeapArray();
+	}
+	
+	public void printHeapArray() {
 		for(int i=0; i<num.length; i++) {
-			System.out.println(num[i] + ',');
+			System.out.println(num[i]);
 		}
 	}
 	
-	
+	public void createHeap() {
+		int n = num.length;
+		for(int i=((n/2)-1); i>=0; i--) {
+			heapify(num, i, n);
+		}
+	}
 	public void heapify(int num[], int i, int n) {
 		Utils utils = new Utils();
 		int largest = i,
@@ -51,4 +63,5 @@ public class Heap {
 			heapify(num, largest, n);
 		}
 	}
+	
 }
