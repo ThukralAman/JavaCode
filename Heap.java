ea85352293b5ca734fd0289fcfab2f57979ebde5
Aman <amanthukral1991@gmail.com>
2017-03-08 20:26:18 +0530

    removing comments

--- Heap.java
+++ Heap.java
@@ -13,20 +13,8 @@ public class Heap {
 	
 	public static void main(String args[]) {
 		Heap h = new Heap();
-		/*int num[] = {33, 2, 198, 89, 1004, 65, 56789, 7, 43, 765};
-		int n = num.length;*/
-		
 		h.printHeapArray();
 		h.createHeap();
-		
-		/*for(int i=0; i<num.length; i++) {
-			System.out.println(num[i] + ',');
-		}*/
-		
-		/*for(int i=((n/2)-1); i>=0; i--) {
-			h.heapify(num, i, n);
-		}*/
-		
 		System.out.println("After heapifying:");
 		System.out.println();
 		h.printHeapArray();
@@ -44,6 +32,7 @@ public class Heap {
 			heapify(num, i, n);
 		}
 	}
+	
 	public void heapify(int num[], int i, int n) {
 		Utils utils = new Utils();
 		int largest = i,
