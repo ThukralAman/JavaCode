153d2e0c8d5f2cf483218fad9eaf4b8e174bd9c3
Aman <amanthukral1991@gmail.com>
2017-09-24 14:33:28 +0530

    stack using Linke dlist
    stack using Linke dlist

---
+++ stack/StackUsinglinkedList.java
@@ -0,0 +1,53 @@
+package stack;
+
+public class StackUsinglinkedList {
+	StackNode top=null;
+	
+	
+	static class StackNode {
+		int data;
+		StackNode next;
+		
+		StackNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+		
+		StackNode() {
+		}
+	}
+	
+	public void push(int data) {
+		StackNode temp = new StackNode(data);
+		temp.next = top;
+		top = temp;
+	}
+	
+	public int pop() {
+		int val = top.data;
+		top = top.next;
+		return val;
+	}
+	
+	public void printStack() {
+		StackNode temp = top;
+		while(temp!=null) {
+			System.out.print(temp.data + ", ");
+			temp = temp.next;
+		}
+	}
+	
+	
+
+	public static void main(String[] args) {
+		StackUsinglinkedList st = new StackUsinglinkedList();
+		if(st.top == null) {
+			System.out.println("null hai");
+		}
+		st.push(10);
+		st.push(20);
+		st.push(30);
+		System.out.println(st.pop());
+		st.printStack();
+	}
+}
